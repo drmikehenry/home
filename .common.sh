79f096b41e940918b186d4072d9a9cbee25f3276
Michael Henry <drmikehenry@drmikehenry.com>
2020-01-04 09:27:17 -0500

    Make pathappend, pathprepend do nothing if new path is already in $PATH.
    This handles the case where $HOME/bin may have already been added to the
    $PATH on some systems.

--- .common.sh
+++ .common.sh
@@ -1,7 +1,17 @@
+# True if $1 contains $2 as a substring.
+__string_contains() {
+    # If removing $2 from $1 makes a change, $1 contains $2.
+    test "${1#*$2*}" != "$1"
+}
+
+pathcontains() {
+    __string_contains ":$PATH:" ":$1:"
+}
+
 pathappend() {
-    test -d "$1" && PATH="$PATH:$1"
+    test -d "$1" && ! pathcontains "$1" && PATH="$PATH:$1"
 }
 
 pathprepend() {
-    test -d "$1" && PATH="$1:$PATH"
+    test -d "$1" && ! pathcontains "$1" && PATH="$1:$PATH"
 }
